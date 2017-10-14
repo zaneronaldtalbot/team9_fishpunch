d4196aac83a7574833d65e1db8fdedc8739f8215
Kazwyrm <tlangford203@gmail.com>
2017-10-14 16:23:52 +1100

    these are the laps scripts. add a lap counter and a lap box to the canvas and attach the lap box to the script.

---
+++ FishPunch Project/TrickyTracks/Assets/TrickyTracks/Scripts/GameManager Scripts/LapsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapsManager : MonoBehaviour {
+
+    public GameObject Lapcounter;
+    [Tooltip("Put in Current checkpoint")]
+    public GameObject currentCheckPoint;
+    [Tooltip("Put in Next Checkpoint")]
+    public GameObject nextCheckpoint;
+
+    public GameObject FinishLine;
+    [HideInInspector]
+    public int lapNumber = 0;
+
+    public void OnTriggerEnter()
+    {
+        if (currentCheckPoint == FinishLine)
+        {
+            lapNumber += 1;
+            currentCheckPoint.SetActive(false);
+            nextCheckpoint.SetActive(true);
+
+
+            Lapcounter.GetComponent<Text>().text = "" + lapNumber;
+        }
+        else
+        {
+            currentCheckPoint.SetActive(false);
+            nextCheckpoint.SetActive(true);
+        }
+    }
+}
