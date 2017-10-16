d35f0a56bddf4e40c2be1c4696bca8ba189020f8
Axel <angycs123@hotmail.com>
2017-10-17 08:48:04 +1100

    added prefabdisabledactor :)

---
+++ FishPunch Project/TrickyTracks/Assets/TrickyTracks/Scripts/GameManager Scripts/PrefabDisabledActor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabDisabledActor : MonoBehaviour {
+
+    private GameObject colliderObject;
+
+    public float timer = 5.0f;
+
+    public bool prefabSet = false;
+
+	// Use this for initialization
+	void Start () {
+        if(this.gameObject.name == "Buzzsaw(Clone)")
+        {
+            colliderObject = this.gameObject.transform.Find("SawBlade_001").gameObject;
+
+
+        }
+        else
+        {
+            colliderObject = this.gameObject.transform.Find("Collider").gameObject;
+        }
+    
+     
+        colliderObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+            timer -= Time.deltaTime;
+        
+
+        if(timer < 0)
+        {
+            colliderObject.SetActive(true);
+        }
+
+
+
+	}
+
+
+
+}
